5e1178bcac85f3716b5777d488171f3c4aabd65c
Ángel Vega <angelantoniovega@gmail.com>
2024-11-15 10:17:36 -0500

    fix: se ajusta variables de entorno

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,7 +1,6 @@
 export const env = {
   IBM_SERVICE_URL: import.meta.env.VITE_IBM_SERVICE_URL,
   IBM_API_KEY: import.meta.env.VITE_IBM_API_KEY,
-  API_URL: import.meta.env.VITE_API_URL,
   GOOGLE_CLIENT_ID: import.meta.env.VITE_GOOGLE_CLIENT_ID,
   CHAT_API_URL: import.meta.env.VITE_CHAT_API_URL,
 
@@ -9,13 +8,12 @@ export const env = {
     const requiredVars = [
       'IBM_SERVICE_URL',
       'IBM_API_KEY',
-      'API_URL',
       'GOOGLE_CLIENT_ID',
       'CHAT_API_URL',
     ];
 
     const missingVars = requiredVars.filter(
-      varName => !env[varName as keyof typeof env]
+      varName => !env[varName as keyof typeof env],
     );
 
     if (missingVars.length > 0) {
--- src/services/ChatService.ts
+++ src/services/ChatService.ts
@@ -1,10 +1,9 @@
 import axios from 'axios';
 import { SendMessageParams, ChatResponse } from '../types/chatResponse';
-
-const API_BASE_URL = 'http://127.0.0.1:5002';
+import { env } from '../config/env';
 
 const api = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: env.CHAT_API_URL,
 });
 
 export const chatService = {
